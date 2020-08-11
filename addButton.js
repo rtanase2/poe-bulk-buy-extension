6ae6cfa2793de6524ecf65de2fb349c1b187d55b
Rachelle Tanase <rachelle.tanase@yahoo.com>
2020-08-10 22:26:51 -0700

    Add more language support
    Missing german and korean

--- addButton.js
+++ addButton.js
@@ -9,22 +9,22 @@ let addedRowListener = false;
 let isExchangeTabActive = window.location.pathname.includes('exchange');
 
 window.addEventListener("DOMSubtreeModified", addRowListenerHandler);
-window.addEventListener('click', function() {
+window.addEventListener('click', function () {
     isExchangeTabActive = document.querySelector('li.active').innerHTML.includes('exchange');
 });
 
 function addRowListenerHandler() {
     if (!addedRowListener) {
         let resultsContainer = document.getElementsByClassName('results');
-        if(resultsContainer.length) {
+        if (resultsContainer.length) {
             addedRowListener = true;
             resultsContainer = resultsContainer[0];
-            resultsContainer.addEventListener("DOMNodeInserted", addButtonHandler);
+            resultsContainer.addEventListener("DOMNodeInserted", addButtonClickListener);
         }
     }
 }
 
-function addButtonHandler(event) {
+function addButtonClickListener(event) {
     const row = event.target;
     // if no data-id then this isn't a valid row to add a button to
     if (row != null && row.getAttribute('data-id')) {
@@ -35,22 +35,18 @@ function addButtonHandler(event) {
         }
 
         const rowID = row.getAttribute('data-id');
-        const characterName = row.querySelector('.character-name').innerText.split(': ')[1];
         const fromPrice = Number(row.querySelector('.price-right > .price-block > span:first-child').innerText);
         const toPrice = Number(row.querySelector('.price-left > .price-block > span:last-child').innerText);
         const toCurrencyType = row.querySelector('.price-left .currency-text').innerText;
         const stock = Number(row.querySelector('.stock > span').innerText);
-        const leagueName = row.querySelector('.status').getAttribute('title');
         const calculatePriceReturn = calculatePrice(stock, fromPrice, toPrice, fromCurrencyType, toCurrencyType);
         const price = calculatePriceReturn[PRICE_KEY];
         const quantityPurchased = calculatePriceReturn[QUANTITY_PURCHASED_KEY];
-        const message = getBulkPurchaseMessage(characterName, quantityPurchased, toCurrencyType, price, fromCurrencyType, leagueName);
         const bulkPurchaseElementID = `${rowID}_bulkPurchaseMessage`;
 
         // add input containing message to copy
         let bulkPurchaseInput = document.createElement('input');
         bulkPurchaseInput.type = 'text';
-        bulkPurchaseInput.value = message;
         bulkPurchaseInput.id = bulkPurchaseElementID;
         bulkPurchaseInput.classList.add('hidden');
         row.append(bulkPurchaseInput);
@@ -60,7 +56,14 @@ function addButtonHandler(event) {
         button.innerHTML = 'Buy All';
         button.classList.add("btn", "btn-default");
         button.onclick = function () {
-            copyBulkPurchaseMessage(bulkPurchaseElementID);
+            if (row.querySelector('textarea') !== null) {
+                const defaultMessage = row.querySelector('textarea').value;
+                const message = getBulkPurchaseMessageV2(defaultMessage, quantityPurchased, price);
+                bulkPurchaseInput.value = message;
+                copyBulkPurchaseMessage(bulkPurchaseElementID);
+            } else {
+                alert('Please press the Contact... button before attempting to buy all');
+            }
         };
         row.querySelector('.btns > .pull-left').append(button);
     }
@@ -115,6 +118,22 @@ function copyBulkPurchaseMessage(elementID) {
 
 function getBulkPurchaseMessage(characterName, stock, toCurrencyType, price, fromCurrencyType, leagueName) {
     return `@${characterName} Hi, I'd like to buy your ${stock} ${toCurrencyType} for my ${price} ${fromCurrencyType} in ${leagueName}.`;
+}
+
+function getBulkPurchaseMessageV2(defaultMessage, stock, price) {
+    // TODO add edge case catch for korean and german
+    // german "'123"
+    // korean "<characters>123"
+    const messageParts = defaultMessage.split(" ");
+    const firstNumberIndex = messageParts.findIndex(stringIsNumber);
+    messageParts[firstNumberIndex] = stock;
+    const secondNumberIndex = messageParts.findIndex(stringIsNumber);
+    messageParts[secondNumberIndex] = price;
+    return messageParts.join(' ');
+}
+
+function stringIsNumber(s) {
+    return String(Number(s)) === s;
 }
 
 function isWholeNumber(num) {
